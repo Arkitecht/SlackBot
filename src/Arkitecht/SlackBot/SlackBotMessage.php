c98626f6b52e0c55178fe4db8a5b9946da3e0c79
Aaron Rubin <aaron@arkitech.net>
2016-06-30 07:56:45 -0400

    Build out Message class

--- src/Arkitecht/SlackBot/SlackBotMessage.php
+++ src/Arkitecht/SlackBot/SlackBotMessage.php
@@ -4,5 +4,61 @@ namespace Arkitecht\SlackBot;
 
 class SlackBotMessage
 {
-    
+    private $keys = [
+        'channel',
+        'username',
+        'text',
+        'icon_emoji',
+        'mrkdwn'
+    ];
+    private $message = [];
+
+    public function __construct($text = '')
+    {
+        if ($text)
+            $this->text = $text;
+    }
+
+    public function attach(SlackBotAttachment $attachment)
+    {
+        if (!array_key_exists('_attachments', $this->message))
+            $this->message['_attachments'] = [];
+        $this->message['_attachments'][] = $attachment;
+    }
+
+    public function setData($data)
+    {
+        foreach ($data as $key => $val)
+            $this->$key = $val;
+    }
+
+    public function __get($name)
+    {
+        if (array_key_exists($name, $this->message))
+            return $this->message[$name];
+
+        return null;
+    }
+
+    public function __set($name, $value)
+    {
+        if (!in_array($name, $this->keys))
+            throw new \Exception("$name is not a valid message key");
+
+        $this->message[$name] = $value;
+    }
+
+    public function __toJson()
+    {
+        $data = $this->message;
+
+        if (array_key_exists('_attachments', $data)) {
+            $data['attachments'] = [];
+            foreach ($data['_attachments'] as $attachment)
+                $data['attachments'][] = $attachment->asJson();
+            unset($data['_attachments']);
+        }
+
+        return json_encode($data);
+    }
 }
